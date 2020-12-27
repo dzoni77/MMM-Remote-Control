2b5584800790554e7c547b68d06301c23cdd76a1
Ezequiel Holzweissig <ezequielgh1999@gmail.com>
2020-12-27 12:54:44 -0300

    Update MMM-Remote-Control.js

--- MMM-Remote-Control.js
+++ MMM-Remote-Control.js
@@ -134,7 +134,7 @@ Module.register("MMM-Remote-Control", {
                 if (!Array.prototype.find) {
                     console.log('Polyfill aplicated!')
                     // https://tc39.github.io/ecma262/#sec-array.prototype.find
-                    Object.defineProperty(Array.prototype,"find",{value:function(r){if(null==this)throw new TypeError('"this" is null or not defined');var e=Object(this),t=e.length>>>0;if("function"!=typeof r)throw new TypeError("predicate must be a function");for(var n=arguments[1],i=0;i<t;){var o=e[i];if(r.call(n,o,i,e))return o;i++}},configurable:!0,writable:!0});
+                    Object.defineProperty(Array.prototype,"find",{value:function(r){if(null===this)throw new TypeError("Array.prototype.find called on null or undefined");if("function"!=typeof r)throw new TypeError("callback must be a function");for(var e=Object(this),n=e.length>>>0,t=arguments[1],o=0;o<n;o++){var f=e[o];if(r.call(f,o,e,t))return f}}});
                 }
                 let i = MM.getModules().find(m => {
                     console.log('find: '+m.identifier)
